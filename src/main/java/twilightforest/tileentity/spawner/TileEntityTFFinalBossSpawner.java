4b46e0bd11dca2028ac01848dab1557f871f23c3
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-05-15 21:33:55 +0200

    Change final boss range, add lightning

--- src/main/java/twilightforest/tileentity/spawner/TileEntityTFFinalBossSpawner.java
+++ src/main/java/twilightforest/tileentity/spawner/TileEntityTFFinalBossSpawner.java
@@ -4,6 +4,7 @@ import com.bobmowzie.mowziesmobs.server.entity.wroughtnaut.EntityWroughtnaut;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.effect.EntityLightningBolt;
 
 public class TileEntityTFFinalBossSpawner extends TileEntityTFBossSpawner {
 
@@ -17,5 +18,16 @@ public class TileEntityTFFinalBossSpawner extends TileEntityTFBossSpawner {
         double movementSpeed = myCreature.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).getBaseValue();
         myCreature.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(movementSpeed * 1.5);
         myCreature.setGlowing(true);
+    }
+
+    @Override
+    protected boolean spawnMyBoss() {
+        this.world.addWeatherEffect(new EntityLightningBolt(this.world, this.getPos().getX(), this.getPos().getY(), this.getPos().getZ(), true));
+        return super.spawnMyBoss();
+    }
+
+    @Override
+    protected int getRange() {
+        return LONG_RANGE;
     }
 }
