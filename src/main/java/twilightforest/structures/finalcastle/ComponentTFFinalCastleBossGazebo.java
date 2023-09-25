3054b8cc781ec51d2a713260fdea094c4df1ae19
ACGaming <4818419+ACGaming@users.noreply.github.com>
2023-09-25 15:36:19 +0200

    Add safety checks to Final Castle generation

--- src/main/java/twilightforest/structures/finalcastle/ComponentTFFinalCastleBossGazebo.java
+++ src/main/java/twilightforest/structures/finalcastle/ComponentTFFinalCastleBossGazebo.java
@@ -9,6 +9,7 @@ import net.minecraft.world.World;
 import net.minecraft.world.gen.structure.StructureBoundingBox;
 import net.minecraft.world.gen.structure.StructureComponent;
 import twilightforest.TFFeature;
+import twilightforest.TwilightForestMod;
 import twilightforest.block.*;
 import twilightforest.enums.BossVariant;
 import twilightforest.structures.StructureTFComponentOld;
@@ -43,33 +44,32 @@ public class ComponentTFFinalCastleBossGazebo extends StructureTFComponentOld {
         int width = 20;
         int height = 5;
 
-        // walls
-        for (Rotation rotation : RotationUtil.ROTATIONS) {
-            this.fillBlocksRotated(world, sbb, 0, 0, 0, 0, height, width, deco.fenceState, rotation);
+        if (deco.blockState.getBlock() == TFBlocks.castle_rune_brick && deco.fenceState.getBlock() == TFBlocks.force_field) {
+            try {
+                // walls
+                for (Rotation rotation : RotationUtil.ROTATIONS) {
+                    this.fillBlocksRotated(world, sbb, 0, 0, 0, 0, height, width, deco.fenceState, rotation);
+                }
+
+                // corner pillars
+                this.fillWithBlocks(world, sbb, 0, 0, 0, 0, height, 0, deco.blockState, deco.blockState, false);
+                this.fillWithBlocks(world, sbb, width, 0, 0, width, height, 0, deco.blockState, deco.blockState, false);
+                this.fillWithBlocks(world, sbb, 0, 0, width, 0, height, width, deco.blockState, deco.blockState, false);
+                this.fillWithBlocks(world, sbb, width, 0, width, width, height, width, deco.blockState, deco.blockState, false);
+
+                // roof
+                this.fillWithBlocks(world, sbb, 0, height + 1, 0, width, height + 1, width, deco.blockState, deco.blockState, false);
+                this.fillWithBlocks(world, sbb, (width / 2) - 1, height + 1, (width / 2) - 1, (width / 2) + 1, height + 1, (width / 2) + 1, deco.fenceState.withProperty(BlockRotatedPillar.AXIS, EnumFacing.Axis.X), deco.fenceState.withProperty(BlockRotatedPillar.AXIS, EnumFacing.Axis.X), false);
+
+                // door
+                IBlockState castleDoor = TFBlocks.castle_door.getDefaultState();
+                this.fillWithBlocks(world, sbb, (width / 2) - 1, 0, 0, (width / 2) + 1, 3, 0, castleDoor.withProperty(BlockTFCastleDoor.LOCK_INDEX, 2), AIR, false);
+            } catch (Exception e) {
+                TwilightForestMod.LOGGER.error("Failed to build final castle boss gazebo");
+            }
         }
 
-        // corner pillars
-        this.fillWithBlocks(world, sbb, 0, 0, 0, 0, height, 0, deco.blockState, deco.blockState, false);
-        this.fillWithBlocks(world, sbb, width, 0, 0, width, height, 0, deco.blockState, deco.blockState, false);
-        this.fillWithBlocks(world, sbb, 0, 0, width, 0, height, width, deco.blockState, deco.blockState, false);
-        this.fillWithBlocks(world, sbb, width, 0, width, width, height, width, deco.blockState, deco.blockState, false);
-
-        // door
-        final IBlockState castleDoor = TFBlocks.castle_door.getDefaultState();
-        this.fillWithBlocks(world, sbb, (width / 2) - 1, 0, 0, (width / 2) + 1, 3, 0, castleDoor.withProperty(BlockTFCastleDoor.LOCK_INDEX, 2), AIR, false);
-
-        // roof
-        this.fillWithBlocks(world, sbb, 0, height + 1, 0, width, height + 1, width, deco.blockState, deco.blockState, false);
-        this.fillWithBlocks(world, sbb, (width / 2) - 1, height + 1, (width / 2) - 1, (width / 2) + 1, height + 1, (width / 2) + 1, deco.fenceState.withProperty(BlockRotatedPillar.AXIS, EnumFacing.Axis.X), deco.fenceState.withProperty(BlockRotatedPillar.AXIS, EnumFacing.Axis.X), false);
-
-        //this.placeSignAtCurrentPosition(world, 10, 0, 10, sbb, "Final Boss Here", "You win!", "discord.gg/6v3z26B");
-
-        //setInvisibleTextEntity(world, 10, 0, 10, sbb, "Final Boss Here", true, 2.3f);
-        //setInvisibleTextEntity(world, 10, 0, 10, sbb, "You win!", true, 2.0f);
-        //setInvisibleTextEntity(world, 10, 0, 10, sbb, "You can join the Twilight Forest Discord server to follow",true, 1.0f);
-        //setInvisibleTextEntity(world, 10, 0, 10, sbb, "the latest updates on this castle and other content at:",true, 0.7f);
-        //setInvisibleTextEntity(world, 10, 0, 10, sbb, "discord.experiment115.com", true, 0.4f);
-
+        // boss spawner
         setBlockState(world, TFBlocks.boss_spawner.getDefaultState().withProperty(BlockTFBossSpawner.VARIANT, BossVariant.FINAL_BOSS), width / 2, 1, width / 2, sbb);
 
         return true;
