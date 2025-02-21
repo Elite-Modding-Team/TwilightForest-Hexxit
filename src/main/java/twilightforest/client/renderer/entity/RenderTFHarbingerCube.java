3d45709362297df5062631f0be776ff737668ee3
Me <135455255+IcarussOne@users.noreply.github.com>
2025-02-21 17:19:46 -0600

    Add explosion ability

--- src/main/java/twilightforest/client/renderer/entity/RenderTFHarbingerCube.java
+++ src/main/java/twilightforest/client/renderer/entity/RenderTFHarbingerCube.java
@@ -4,26 +4,81 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import twilightforest.TwilightForestMod;
 import twilightforest.client.model.entity.ModelTFApocalypseCube;
 import twilightforest.entity.finalcastle.EntityTFHarbingerCube;
 
 public class RenderTFHarbingerCube extends RenderLiving<EntityTFHarbingerCube> {
 
-	private static final ResourceLocation textureLoc = TwilightForestMod.getModelTexture("apocalypse2.png");
+    private static final ResourceLocation textureLoc = TwilightForestMod.getModelTexture("apocalypse2.png");
 
-	public RenderTFHarbingerCube(RenderManager manager) {
-		super(manager, new ModelTFApocalypseCube(), 1.0F);
-	}
+    public RenderTFHarbingerCube(RenderManager manager) {
+        super(manager, new ModelTFApocalypseCube(), 1.0F);
+    }
 
-	@Override
-	protected ResourceLocation getEntityTexture(EntityTFHarbingerCube entity) {
-		return textureLoc;
-	}
+    @Override
+    protected ResourceLocation getEntityTexture(EntityTFHarbingerCube entity) {
+        return textureLoc;
+    }
 
-	@Override
-	protected void preRenderCallback(EntityTFHarbingerCube entity, float partialTicks) {
-		float scale = 1.0F;
-		GlStateManager.scale(scale, scale, scale);
-	}
+    @Override
+    protected void preRenderCallback(EntityTFHarbingerCube entity, float partialTicks) {
+        float bounce = entity.ticksExisted + partialTicks;
+        GlStateManager.translate(0F, MathHelper.sin((bounce) * 0.2F) * 0.15F, 0F);
+
+        // flash
+
+        float f1 = entity.deathTime;
+        if (f1 > 0) {
+            float f2 = 1.0F + MathHelper.sin(f1 * 100.0F) * f1 * 0.01F;
+
+            if (f1 < 0.0F) {
+                f1 = 0.0F;
+            }
+
+            if (f1 > 1.0F) {
+                f1 = 1.0F;
+            }
+
+            f1 *= f1;
+            f1 *= f1;
+            float f3 = (1.0F + f1 * 0.4F) * f2;
+            float f4 = (1.0F + f1 * 0.1F) / f2;
+            GlStateManager.scale(f3, f4, f3);
+        }
+    }
+
+    @Override
+    protected void applyRotations(EntityTFHarbingerCube entity, float ageInTicks, float rotationYaw, float partialTicks) {
+        GlStateManager.rotate(180.0F - rotationYaw, 0.0F, 1.0F, 0.0F);
+    }
+
+    @Override
+    protected int getColorMultiplier(EntityTFHarbingerCube entity, float brightness, float partialTicks) {
+        if (entity.deathTime > 0) {
+            float f2 = entity.deathTime + partialTicks;
+
+            if ((int) (f2 / 2) % 2 == 0) {
+                return 0;
+            } else {
+                int i = (int) (f2 * 0.2F * 255.0F);
+
+                if (i < 0) {
+                    i = 0;
+                }
+
+                if (i > 255) {
+                    i = 255;
+                }
+
+                short short1 = 255;
+                short short2 = 255;
+                short short3 = 255;
+                return i << 24 | short1 << 16 | short2 << 8 | short3;
+            }
+        } else {
+            return 0;
+        }
+    }
 }
--- src/main/java/twilightforest/entity/finalcastle/EntityTFHarbingerCube.java
+++ src/main/java/twilightforest/entity/finalcastle/EntityTFHarbingerCube.java
@@ -1,5 +1,9 @@
 package twilightforest.entity.finalcastle;
 
+import java.util.List;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILookIdle;
@@ -9,38 +13,68 @@ import net.minecraft.entity.ai.EntityAIWanderAvoidWater;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import net.minecraftforge.event.ForgeEventFactory;
+import twilightforest.TFSounds;
 import twilightforest.TwilightForestMod;
 
 public class EntityTFHarbingerCube extends EntityMob {
+    public static final ResourceLocation LOOT_TABLE = TwilightForestMod.prefix("entities/harbinger_cube");
+    private static final float EXPLOSION_RADIUS = 1.0F;
 
-	public static final ResourceLocation LOOT_TABLE = TwilightForestMod.prefix("entities/harbinger_cube");
+    public EntityTFHarbingerCube(World world) {
+        super(world);
+        this.setSize(1.9F, 2.4F);
+    }
 
-	public EntityTFHarbingerCube(World world) {
-		super(world);
-		this.setSize(1.9F, 2.4F);
-	}
+    @Override
+    protected void initEntityAI() {
+        this.tasks.addTask(1, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIWanderAvoidWater(this, 1.0D));
+        this.tasks.addTask(2, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
+        this.tasks.addTask(2, new EntityAILookIdle(this));
+        this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));
+        this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<>(this, EntityPlayer.class, true));
+    }
 
-	@Override
-	protected void initEntityAI() {
-		this.tasks.addTask(1, new EntityAISwimming(this));
-		this.tasks.addTask(1, new EntityAIWanderAvoidWater(this, 1.0D));
-		this.tasks.addTask(2, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
-		this.tasks.addTask(2, new EntityAILookIdle(this));
-		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));
-		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget<>(this, EntityPlayer.class, true));
-	}
+    @Override
+    protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(60.0D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.23D);
+    }
 
-	@Override
-	protected void applyEntityAttributes() {
-		super.applyEntityAttributes();
-		this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(50.0D);
-		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.23D);
-	}
+    @Override
+    protected void onDeathUpdate() {
+        ++this.deathTime;
 
-	@Override
-	protected ResourceLocation getLootTable() {
-		return LOOT_TABLE;
-	}
+        if (this.deathTime == 60) // delay until 3 seconds
+        {
+            if (!world.isRemote) {
+                List<Entity> list = EntityTFHarbingerCube.this.world.getEntitiesWithinAABBExcludingEntity((Entity) EntityTFHarbingerCube.this, EntityTFHarbingerCube.this.getEntityBoundingBox().grow(4.0D));
+
+                boolean mobGriefing = ForgeEventFactory.getMobGriefingEvent(world, this);
+                this.world.createExplosion(this, this.posX, this.posY, this.posZ, EntityTFHarbingerCube.EXPLOSION_RADIUS, mobGriefing);
+                this.playSound(TFSounds.FINALBOSS_DEATH, 2.5F, 3.0F);
+
+                for (Entity entity : list) {
+                    if (entity instanceof EntityLivingBase) {
+                        ((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.WITHER, 20 * 15, 0));
+                    }
+                }
+            }
+            // Fake to trigger super's behaviour
+            deathTime = 19;
+            super.onDeathUpdate();
+            deathTime = 60;
+        }
+    }
+
+    @Override
+    protected ResourceLocation getLootTable() {
+        return LOOT_TABLE;
+    }
 }
